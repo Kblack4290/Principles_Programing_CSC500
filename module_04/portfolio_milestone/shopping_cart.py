f3f1a0b11c39be4c88ccc77d1a0fe4e8f5e0ee78
Keith Black <Keithblack4290@gmail.com>
2026-05-10 17:37:22 -0600

    answers in line with question

--- module_04/portfolio_milestone/shopping_cart.py
+++ module_04/portfolio_milestone/shopping_cart.py
@@ -7,7 +7,7 @@ class ItemToPurchase:
     # Method to calculate and print the total cost of the item
     def print_item_cost(self):
         total = self.item_price * self.item_quantity
-        print(f"\033[1;35m {self.item_name} {self.item_quantity} @ ${self.item_price:.2f} = ${total:.2f} \033[0m")
+        print(f"\033[1;35m {self.item_name} {self.item_quantity} @ ${self.item_price:.2f} = ${total:.2f} \033[0m".center(50))
         return total
     
 # initialize an empty list to hold the items in the shopping cart, a boolean variable to control the while loop, and a counter to keep track of the number of items added to the cart
@@ -18,15 +18,16 @@ items_added = 0
 # while loop to allow user to add items to the shopping cart until they choose to stop adding items
 while add_item:
     # Prompt the user to add an item to the shopping cart
-    input_item = input("Do you want to add an item to the shopping cart? (yes/no)\n")
+    input_item = input("Do you want to add an item to the shopping cart? (yes/no) ")
     
     # If the user inputs 'yes', prompt them to enter the item name, price, and quantity.
     # Ensure that the user inputs valid data for price and quantity. 
     if input_item.lower() == 'yes':
-        name = input("Enter the item name:\n")
+        print(f"Item {items_added + 1}")
+        name = input("Enter the item name: ")
         try:
-            price = float(input("Enter the item price:\n"))
-            quantity = int(input("Enter the item quantity:\n"))
+            price = float(input("Enter the item price: "))
+            quantity = int(input("Enter the item quantity: "))
         except ValueError:
             print("\033[31m Invalid input. Price and quantity must be a number. \033[0m")
             continue
@@ -48,13 +49,13 @@ while add_item:
         print("\033[31m Invalid input. Please enter 'yes' or 'no'. \033[0m")
 
 # After the user has finished adding items to the shopping cart, print out the total cost of all items in the cart by calling the print_item_cost() method for each item and summing the total cost.
-print("\033[1;35m TOTAL COST: \033[0m")
+print("\033[1;35m TOTAL COST: \033[0m".center(50))
 
 total_cost = 0
 for item in items:
     total_cost += item.print_item_cost()
 
-print(f"\033[1;35m Total: ${total_cost:.2f} \033[0m")
+print(f"\033[1;35m Total: ${total_cost:.2f} \033[0m".center(50))
 
 
 
